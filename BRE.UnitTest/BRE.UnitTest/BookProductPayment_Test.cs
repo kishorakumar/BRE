1e5b048b0eb448817aa101db0df6df9e8b2c008b
Kishora Kumar <kishor.mittur@gmail.com>
2020-06-09 10:28:59 +0530

    Refactoring: Changed the Test Cases for Book Products to use Facade and removed code from BookProuct model

--- BRE.UnitTest/BRE.UnitTest/BookProductPayment_Test.cs
+++ BRE.UnitTest/BRE.UnitTest/BookProductPayment_Test.cs
@@ -3,14 +3,17 @@ using BRE.Core;
 using BRE.Core.Models;
 using Moq;
 using System.Collections.Generic;
+using BRE.Core.Actions;
 
 namespace BRE.UnitTest
 {
     public class BookProductPayment_Test
     {
+        private IPostPaymentActions postPaymentHandler;
         [SetUp]
         public void Setup()
         {
+            
         }
 
         [Test]
@@ -18,9 +21,31 @@ namespace BRE.UnitTest
         {
             IProduct physicalProduct = new ProductBook();
 
-            physicalProduct.ProductName = "Good Book";
+            physicalProduct.ProductName = "Basket";
 
-            List<string> statuses = physicalProduct.PaymentDone();
+            var mockPackagingSlipGenerator = new Mock<IGeneratePackagingSlip>();
+
+            var mockComisionGenerator = new Mock<IGenerateCommisionToAgent>();
+            mockComisionGenerator.Setup(m => m.GeneratePackingCommisionToAgent(physicalProduct))
+                                    .Returns("Generated Commision To Agent");
+
+            var mockDuplicatePackagingSlipGenerator = new Mock<IGenerateDuplicatePackingSlipForRoyalty>();
+            mockDuplicatePackagingSlipGenerator.Setup(m => m.GenerateDuplicatePackingSlipForRoyalty(physicalProduct))
+                                    .Returns("Generated Duplicate packing slip for the royalty department");
+
+            var mockMembershipNew = new Mock<IActivateMembership>();
+            var mockMembershipUpgrade = new Mock<IUpgradeMembership>();
+            var mockEmailSender = new Mock<ISendEmail>();
+            var mockAddFreeProducts = new Mock<IAddEligibleFreeProducts>();
+
+            postPaymentHandler = new PostPaymentActions(mockMembershipNew.Object, mockAddFreeProducts.Object,
+                                                   mockComisionGenerator.Object, mockDuplicatePackagingSlipGenerator.Object,
+                                                   mockPackagingSlipGenerator.Object, mockMembershipUpgrade.Object,
+                                                   mockEmailSender.Object);
+
+            List<IProduct> products = new List<IProduct>() { physicalProduct };
+            List<string> statuses = postPaymentHandler.PerformPostPaymentActions(products);
+
 
             Assert.IsTrue(statuses.Contains("Generated Duplicate packing slip for the royalty department"));
         }
@@ -30,9 +55,30 @@ namespace BRE.UnitTest
         {
             IProduct physicalProduct = new ProductBook();
 
-            physicalProduct.ProductName = "Good Book";
+            physicalProduct.ProductName = "Basket";
 
-            List<string> statuses = physicalProduct.PaymentDone();
+            var mockPackagingSlipGenerator = new Mock<IGeneratePackagingSlip>();
+
+            var mockComisionGenerator = new Mock<IGenerateCommisionToAgent>();
+            mockComisionGenerator.Setup(m => m.GeneratePackingCommisionToAgent(physicalProduct))
+                                    .Returns("Generated Commision To Agent");
+
+            var mockDuplicatePackagingSlipGenerator = new Mock<IGenerateDuplicatePackingSlipForRoyalty>();
+            mockDuplicatePackagingSlipGenerator.Setup(m => m.GenerateDuplicatePackingSlipForRoyalty(physicalProduct))
+                                    .Returns("Generated Duplicate packing slip for the royalty department");
+
+            var mockMembershipNew = new Mock<IActivateMembership>();
+            var mockMembershipUpgrade = new Mock<IUpgradeMembership>();
+            var mockEmailSender = new Mock<ISendEmail>();
+            var mockAddFreeProducts = new Mock<IAddEligibleFreeProducts>();
+
+            postPaymentHandler = new PostPaymentActions(mockMembershipNew.Object, mockAddFreeProducts.Object,
+                                                   mockComisionGenerator.Object, mockDuplicatePackagingSlipGenerator.Object,
+                                                   mockPackagingSlipGenerator.Object, mockMembershipUpgrade.Object,
+                                                   mockEmailSender.Object);
+
+            List<IProduct> products = new List<IProduct>() { physicalProduct };
+            List<string> statuses = postPaymentHandler.PerformPostPaymentActions(products);
 
             Assert.IsTrue(statuses.Contains("Generated Commision To Agent"));
         }
@@ -42,9 +88,31 @@ namespace BRE.UnitTest
         {
             IProduct physicalProduct = new ProductBook();
 
-            physicalProduct.ProductName = "Good Book";
+            physicalProduct.ProductName = "Basket";
 
-            List<string> statuses = physicalProduct.PaymentDone();
+            var mockPackagingSlipGenerator = new Mock<IGeneratePackagingSlip>();
+
+            var mockComisionGenerator = new Mock<IGenerateCommisionToAgent>();
+            mockComisionGenerator.Setup(m => m.GeneratePackingCommisionToAgent(physicalProduct))
+                                    .Returns("Generated Commision To Agent");
+
+            var mockDuplicatePackagingSlipGenerator = new Mock<IGenerateDuplicatePackingSlipForRoyalty>();
+            mockDuplicatePackagingSlipGenerator.Setup(m => m.GenerateDuplicatePackingSlipForRoyalty(physicalProduct))
+                                    .Returns("Generated Duplicate packing slip for the royalty department");
+
+            var mockMembershipNew = new Mock<IActivateMembership>();
+            var mockMembershipUpgrade = new Mock<IUpgradeMembership>();
+            var mockEmailSender = new Mock<ISendEmail>();
+            var mockAddFreeProducts = new Mock<IAddEligibleFreeProducts>();
+
+            postPaymentHandler = new PostPaymentActions(mockMembershipNew.Object, mockAddFreeProducts.Object,
+                                                   mockComisionGenerator.Object, mockDuplicatePackagingSlipGenerator.Object,
+                                                   mockPackagingSlipGenerator.Object, mockMembershipUpgrade.Object,
+                                                   mockEmailSender.Object);
+
+            List<IProduct> products = new List<IProduct>() { physicalProduct };
+            List<string> statuses = postPaymentHandler.PerformPostPaymentActions(products);
+
 
             Assert.IsTrue(statuses.Contains("Generated Commision To Agent"));
             Assert.IsTrue(statuses.Contains("Generated Duplicate packing slip for the royalty department"));
